2b59f46e5e095abb914ce77a2bf58c35b4c28202
TylerLK <tylerLK2003@gmail.com>
2026-07-14 21:09:54 -0400

    Revert "Part 1 Code"
    This reverts commit 721a69b29bbea55aedfc485aeae7a29e127e8901.

--- src/main.cpp
+++ src/main.cpp
@@ -2,64 +2,8 @@
 
 using namespace std;
 
-int* createArray(int arraySize);
-void initializeArray(int* array, int arraySize);
-void printArray(int* array, int arraySize);
-void deleteArray(int* array);
-
-int main() {
-	bool isRunning{ true };
-	int arrSize{ 1 };
-
-	while (isRunning) {
-		cout << "Enter the size of your array (or '0' to stop the loop): ";
-		cin >> arrSize;
-
-		if (arrSize > 0) {
-			// Create the array based on user input
-			int* arr = createArray(arrSize);
-			cout << "Array of size " << arrSize << " created at: " << arr << endl;
-
-			// Initialize the array with values from '0' to 'arrSize - 1'
-			initializeArray(arr, arrSize);
-			cout << "Array initialized with values from 0 to " << arrSize - 1 << ":" << endl;
-
-			// Print the values of the array
-			printArray(arr, arrSize);
-
-			// De-allocate the array's dynamically-allocated memory.
-			deleteArray(arr);
-			cout << "The array has been deleted from memory." << endl;
-		}
-		else {
-			isRunning = false;
-		}
-	}
-
-	cout << "Exiting program...";
-
+int main()
+{
+	cout << "Hello World" << endl;
 	return 0;
-}
-
-int* createArray(int arraySize) {
-	int* array = (int*)malloc(arraySize * sizeof(int));
-
-	return array;
-}
-
-void initializeArray(int* array, int arraySize) {
-	for (int i = 0; i < arraySize; i++) {
-		*(array + i) = i;
-	}
-}
-
-void printArray(int* array, int arraySize) {
-	for (int i = 0; i < arraySize; i++) {
-		cout << *(array + i) << " ";
-	}
-	cout << endl;
-}
-
-void deleteArray(int* array) {
-	free(array);
 }
