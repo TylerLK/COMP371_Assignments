84b4281a738864dfbfb36152799580c96e342d8a
Michael Ziegahn <ziegahnmichael@gmail.com>
2026-07-16 14:21:33 -0400

    Implement driver class
    Creates Triangle, Translates the triangle, and DIsplays it

--- Assignment1/Part2/src/Driver.h
+++ Assignment1/Part2/src/Driver.h
@@ -1,10 +1,28 @@
 #include "Triangle.h"
-
 using namespace std;
 
 class Driver {
 public:
+	// Constructor
+	Driver() : triangle(nullptr) {}
 
+	void translate(int d, char axis) {
+		if (triangle != nullptr) {
+			triangle->translate(d, axis);
+		}
+	}
+
+	~Driver() {
+		delete triangle;
+	}
+
+	void createTriangle(Point p1, Point p2, Point p3) {
+		triangle = new Triangle(p1, p2, p3);
+	}
+
+	void display() {
+		if (triangle != nullptr) { triangle->display(); }
+	}
 private:
-
+	Triangle* triangle;
 };
